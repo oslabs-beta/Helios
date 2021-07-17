9db24eb366db4a1f9a34cba3797fdf2e1e094f9e
prasad-pul <prasad.pul@gmail.com>
2021-07-17 17:14:30 -0400

    CloudWatch Metrics Pull initial version 20210717

---
+++ src/aws_metrics/cw_lambda_metrics.js
@@ -0,0 +1,158 @@
+const AWS = require('aws-sdk');
+
+AWS.config.credentials = new AWS.SharedIniFileCredentials({
+  profile: 'work-account',
+});
+
+var sts = new AWS.STS({
+  apiVersion: '2011-06-15',
+  httpOptions: { xhrAsync: false },
+});
+
+AWS.config.update({ region: 'us-east-2' });
+
+const roleParams = {
+  RoleArn: 'arn:aws:iam::142167254676:role/HeliosDelegationRole',
+  RoleSessionName: 'testingSessionWork',
+};
+
+const assumedRole = sts.assumeRole(roleParams, function (err, data) {
+  if (err) console.log('ERROR IN ASSUME ROLE: ', err, err.stack);
+  else console.log('Assumed Role Data: ', data, '\n\n');
+});
+
+assumedRole.on('complete', function (response) {
+  const accessKeyId = response.data.Credentials.AccessKeyId;
+  const secretAccessKey = response.data.Credentials.SecretAccessKey;
+  const sessionToken = response.data.Credentials.SessionToken;
+  const credentials = { accessKeyId, secretAccessKey, sessionToken };
+  var lambda = new AWS.Lambda({
+    credentials,
+  });
+
+  var lamParams = { FunctionVersion: 'ALL' };
+  let funcNames = [];
+
+  lambda.listFunctions(lamParams, function (err, data) {
+    if (err) console.log('ERROR IN LIST FUNCTIONS', err, err.stack);
+    else {
+      funcNames = data.Functions.map((el) => el.FunctionName);
+      const cw = new AWS.CloudWatch({ apiVersion: '2010-08-01', credentials });
+
+      let cw_metrics = [
+        {
+          name: 'Invocations',
+          Stat: 'Sum',
+        },
+        {
+          name: 'Errors',
+          Stat: 'Sum',
+        },
+        {
+          name: 'Throttles',
+          Stat: 'Sum',
+        },
+      ];
+
+      //input time range period for aggregating the metrics
+      //for e.g. if the time range selected on the front end is minutes, metrics from CloudWatch would be
+      // aggregated by 1 minute (60 seconds)
+
+      const timeRangePeriod = {
+        minutes: 60, //60 seconds
+        hours: 300, //300 secs
+        days: 3600, // 1 hour
+      };
+
+      //rouding parameters for defining the EndTime
+
+     const timeRoundUnits = {
+        minutes: 5, //the EndTime time stamps will be rounded to nearest 5 minutes
+        hours: 15, //rounded to nearest 15 minutes
+        days: 60, // rounded to nearest hour
+      };
+
+      
+
+
+
+
+      //inputs from the FrontEnd
+      let period = 2 * 7 * 24 * 3600; // Two Weeks in seconds: this will be a input from the Front End
+      let interval = 3600; //1 hr in secs: this will be a input from the Front End (graph unit)
+
+      //define the End and Start times in UNIX time Stamp format for getMetricsData method
+      //Rounded off to nearest 15 min
+      let EndTime = Math.round(new Date().getTime() / 1000 / 60 / 15) * 60 * 15; //current time in Unix TimeStamp
+      let StartTime = EndTime - period;
+
+      //initialize the parameters
+      let getMetricsParams = {
+        StartTime,
+        EndTime,
+        LabelOptions: {
+          Timezone: '-0400',
+        },
+        MetricDataQueries: [],
+      };
+      //initiate a counter
+      let counter = 0;
+
+      //programatically update the data queries array of the parameters
+      for (metric of cw_metrics) {
+        console.log(metric);
+        funcNames.forEach((func) => {
+          let MetricDataQuery = {
+            Id: `m${counter}`,
+            Label: `Lambda ${metric.name} ${func}`,
+            MetricStat: {
+              Metric: {
+                Namespace: 'AWS/Lambda',
+                MetricName: `${metric.name}`,
+                Dimensions: [
+                  {
+                    Name: `FunctionName`,
+                    Value: `${func}`,
+                  },
+                ],
+              },
+              Period: timeRangePeriod,
+              Stat: `${metric.Stat}`,
+            },
+          };
+          getMetricsParams.MetricDataQueries.push(MetricDataQuery); //Add the Data queries for the getMetricsData method
+          counter++;
+        });
+      }
+
+      cw.getMetricData(getMetricsParams, function (err, data) {
+        if (err) {
+          console.log('Error in CW getMetricsData', err);
+        } else {
+          console.log('Get Metrics Data:  ', JSON.stringify(data, null, 2));
+        }
+      });
+
+      console.log(
+        'LAMBDA DATA FROM THE ACCOUNT WE ASSUMED A NEW ROLE FOR: ',
+        funcNames,
+        '\n**************\n'
+      );
+    }
+  });
+});
+
+let outsideLambda = new AWS.Lambda();
+
+let outsideLamParams = { FunctionVersion: 'ALL', MaxItems: 5 };
+
+outsideLambda.listFunctions(outsideLamParams, function (err, data) {
+  if (err) console.log('ERROR IN LIST FUNCTIONS', err, err.stack);
+  else
+    console.log(
+      '\n******------******\n',
+      'Data from the Parent Lambda Functions: ',
+      data,
+      '\n******------******\n'
+    );
+});
