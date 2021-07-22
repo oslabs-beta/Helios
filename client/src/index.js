cb07fef4ab347f107968f7a6685f37a44e72067f
theingideva <theingideva@gmail.com>
2021-07-22 14:03:43 -0400

    indexedDB with mocked data

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import App from './App.jsx';
-import store from './store.js';
+import React, { Component } from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import App from "./App.jsx";
+import store from "./store.js";
+import "./indexedDB/mainIdb";
 
 render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
