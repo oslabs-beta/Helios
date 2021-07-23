6fe2c41ab5d2a4d68e84df66d997e63512736e7b
theingideva <theingideva@gmail.com>
2021-07-23 13:09:01 -0400

    name, email sign up page are stored in indexeddb

--- client/src/indexedDB/mainIdb.js
+++ client/src/indexedDB/mainIdb.js
@@ -1,35 +1,26 @@
 import Dexie from "dexie";
-//
-// Define your database
-//
-var db = new Dexie("helios_database");
 
-db.version(1).stores({
-  lambdas: "invocations,  metrics",
-});
+let db = new Dexie("helios_database");
 
-//
-// Put some data into it
-//
-db.lambdas
-  .put({ invocations: "invocationsArr", metrics: "metricsArr" })
-  .then(function () {
-    //
-    // Then when data is stored, read from it
-    //
-    return db.lambdas.get("invocationsArr");
-  })
-  .then(function (lambda) {
-    //
-    // Display the result
-    //
-    alert("This is the log " + lambda.invocations);
-  })
-  .catch(function (error) {
-    //
-    // Finally don't forget to catch any error
-    // that could have happened anywhere in the
-    // code blocks above.
-    //
-    console.log(error, "error")
-  });
+// db.lambdas
+//   .put({ invocations: "invocationsArr", metrics: "metricsArr" })
+//   .then(function () {
+//     // Then when data is stored, read from it
+//     return db.lambdas.get("invocationsArr");
+//   })
+//   .then(function (lambda) {
+//     //
+//     // Display the result
+//     //
+//     console.log("This is the log " + lambda.invocations);
+//   })
+//   .catch(function (error) {
+//     //
+//     // Finally don't forget to catch any error
+//     // that could have happened anywhere in the
+//     // code blocks above.
+//     //
+//     console.log(error, "error");
+//   });
+
+export default db;
