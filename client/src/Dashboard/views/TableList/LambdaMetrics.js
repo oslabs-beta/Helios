9cbd5056b34b8b2a3659955d3af2ed8333e4a6be
prasad-pul <prasad.pul@gmail.com>
2021-08-02 11:27:41 -0400

    Prasad Dashboard Working Component, need to fix the async logic to fetch the Dashboard metrics

--- client/src/Dashboard/views/TableList/LambdaMetrics.js
+++ client/src/Dashboard/views/TableList/LambdaMetrics.js
@@ -5,25 +5,24 @@ const columns = [
   { field: 'id', headerName: 'ID', width: 90, hide: true },
   {
     field: 'functionName',
-    headerName: 'Lambda Function Name', 
+    headerName: 'Lambda Function Name',
     width: 250,
     editable: true,
   },
   {
     field: 'throttles',
     headerName: 'Throttles',
-    type: 'number',    
+    type: 'number',
     width: 150,
     editable: true,
   },
   {
     field: 'invocations',
     headerName: 'Invocations',
-    type: 'number',    
+    type: 'number',
     width: 150,
     editable: true,
   },
-
   {
     field: 'errors',
     headerName: 'Errors',
@@ -31,32 +30,37 @@ const columns = [
     width: 150,
     editable: true,
   },
-
 ];
 
-
-
 export default function DataTable(props) {
-
   let rows = [];
-  console.log(props)
+  console.log(props);
 
-  let invocationsArr = props.invocations.series.map((funcData) => funcData.total)
-  let throttlesArr = props.throttles.series.map((funcData) => funcData.total)
-  let errorsArr = props.errors.series.map((funcData) => funcData.total)
+  let invocationsArr = props.invocations.series.map(
+    (funcData) => funcData.total
+  );
+  let throttlesArr = props.throttles.series.map((funcData) => funcData.total);
+  let errorsArr = props.errors.series.map((funcData) => funcData.total);
 
   props.funcNames.forEach((func, index) => {
     let rowData = {
       id: index,
       functionName: func,
-      throttles: throttlesArr[index],
       invocations: invocationsArr[index],
+      throttles: throttlesArr[index],
       errors: errorsArr[index],
-      
-     }
-     rows.push(rowData)
+    };
+    rows.push(rowData);
+  });
 
-
-  })
-
+  return (
+    <div style={{ height: 300, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        pageSize={20}
+        disableSelectionOnClick
+      />
+    </div>
+  );
 }
