03ff24b2dcbcc07a2512836267186e96cc01f242
jackiedouglass <jackie.douglass@icloud.com>
2021-08-03 18:55:29 -0500

    Working version before building Electron app

--- client/src/Dashboard/routes.js
+++ client/src/Dashboard/routes.js
@@ -20,17 +20,11 @@ import Dashboard from '@material-ui/icons/Dashboard';
 import Person from '@material-ui/icons/Person';
 import LibraryBooks from '@material-ui/icons/LibraryBooks';
 import CallSplit from '@material-ui/icons/CallSplit';
-import BubbleChart from '@material-ui/icons/BubbleChart';
-import LocationOn from '@material-ui/icons/LocationOn';
-import Notifications from '@material-ui/icons/Notifications';
-import Unarchive from '@material-ui/icons/Unarchive';
-import Language from '@material-ui/icons/Language';
+
 // core components/views for Admin layout
 import DashboardPage from './views/Dashboard/Dashboard.js';
 import UserProfile from './views/UserProfile/UserProfile.js';
-// import TableList from './views/TableList/TableList.js';
-// import Typography from './views/Typography/Typography.js';
-import NotificationsPage from './views/Notifications/Notifications.js';
+
 import Logs from './views/Logs/Logs';
 import APIGateway from './views/APIGateway/APIGateway.js';
 // core components/views for RTL layout
@@ -64,54 +58,6 @@ const dashboardRoutes = [
     component: UserProfile,
     layout: '/admin',
   },
-  // {
-  //   path: '/table',
-  //   name: 'Table List',
-  //   rtlName: 'قائمة الجدول',
-  //   icon: 'content_paste',
-  //   component: TableList,
-  //   layout: '/admin',
-  // },
-  // {
-  //   path: '/typography',
-  //   name: 'Typography',
-  //   rtlName: 'طباعة',
-  //   icon: LibraryBooks,
-  //   component: Typography,
-  //   layout: '/admin',
-  // },
-  // {
-  //   path: '/icons',
-  //   name: 'Icons',
-  //   rtlName: 'الرموز',
-  //   icon: BubbleChart,
-  //   component: Icons,
-  //   layout: '/admin',
-  // },
-  // {
-  //   path: '/notifications',
-  //   name: 'Notifications',
-  //   rtlName: 'إخطارات',
-  //   icon: Notifications,
-  //   component: NotificationsPage,
-  //   layout: '/admin',
-  // },
-  // {
-  //   path: '/rtl-page',
-  //   name: 'RTL Support',
-  //   rtlName: 'پشتیبانی از راست به چپ',
-  //   icon: Language,
-  //   component: RTLPage,
-  //   layout: '/rtl',
-  // },
-  // {
-  //   path: '/upgrade-to-pro',
-  //   name: 'Upgrade To PRO',
-  //   rtlName: 'التطور للاحترافية',
-  //   icon: Unarchive,
-  //   component: UpgradeToPro,
-  //   layout: '/admin',
-  // },
 ];
 
 export default dashboardRoutes;
